dff38ae6166d0200b4ba54bb48849dac8f18963e
maldives0 <maliethy@gmail.com>
2021-02-11 19:56:20 +0900

    doItAndroid tutorial

---
+++ DoItAndroid/app/src/main/java/com/example/doitandroid/MenuActivity.java
@@ -0,0 +1,29 @@
+package com.example.doitandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button button2 = (Button) findViewById(R.id.btn_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("name", "mike");
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
